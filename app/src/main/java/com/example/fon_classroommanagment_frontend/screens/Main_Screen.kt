f1ccebac034a3deab302d705b194f08a784d3d9c
ilijaradojkovic <47418955+ilijaradojkovic@users.noreply.github.com>
2022-06-06 14:09:56 +0200

    Dodata search animacija

--- app/src/main/java/com/example/fon_classroommanagment_frontend/screens/Main_Screen.kt
+++ app/src/main/java/com/example/fon_classroommanagment_frontend/screens/Main_Screen.kt
@@ -7,8 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -28,6 +27,7 @@ fun Main_Screen(navHostController: NavHostController, Title: String){
     val coroutineScope = rememberCoroutineScope()
     val modalBottomSheetState = rememberModalBottomSheetState(initialValue = ModalBottomSheetValue.Hidden)
 
+    var displayTopBarElements by remember{mutableStateOf(false)}
     ModalBottomSheetLayout(sheetState = modalBottomSheetState,
 
         sheetContent =  {
@@ -37,7 +37,8 @@ fun Main_Screen(navHostController: NavHostController, Title: String){
        sheetBackgroundColor = Color.Transparent,
 
     ) {
-        Scaffold(topBar = { TopBar(false,{coroutineScope.launch { if(modalBottomSheetState.targetValue==ModalBottomSheetValue.Expanded) modalBottomSheetState.hide() else modalBottomSheetState.show()}}) }, bottomBar = { BottonBar(navHostController) }) {
+        Scaffold(topBar = { TopBar(displayTopBarElements,{coroutineScope.launch { if(modalBottomSheetState.targetValue==ModalBottomSheetValue.Expanded) modalBottomSheetState.hide() else modalBottomSheetState.show()}}) },
+            bottomBar = { BottonBar(navHostController) }) {
             Column(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -47,12 +48,15 @@ fun Main_Screen(navHostController: NavHostController, Title: String){
 
                 when(Title){
                     "Classrooms"->{
+                        displayTopBarElements=false
                         All_Classrooms(navHostController)
                     }
                     "Reservations"->{
+                        displayTopBarElements=true
                         AllReservations_Screen(navHostController)
                     }
                     "Profile"->{
+                        displayTopBarElements=true
                         Profile_Screen(isAdmin = false, fullName ="Ilija Radojkovic" )
                     }
                 }
