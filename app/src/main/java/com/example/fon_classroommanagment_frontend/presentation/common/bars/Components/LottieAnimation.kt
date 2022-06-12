53feb2f0efa3e0a172c738a28c91ffe9f72d1e4a
ilijaradojkovic <47418955+ilijaradojkovic@users.noreply.github.com>
2022-06-12 15:54:52 +0200

    Splash screen animacija dodata,uradjena ikona aplikacije

--- app/src/main/java/com/example/fon_classroommanagment_frontend/presentation/common/bars/Components/LottieAnimation.kt
+++ app/src/main/java/com/example/fon_classroommanagment_frontend/presentation/common/bars/Components/LottieAnimation.kt
@@ -5,20 +5,24 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.airbnb.lottie.compose.LottieAnimation
-import com.airbnb.lottie.compose.LottieCompositionSpec
-import com.airbnb.lottie.compose.LottieConstants
-import com.airbnb.lottie.compose.rememberLottieComposition
+import com.airbnb.lottie.compose.*
 
 @Composable
-fun LottieAnimation( lottieAnim:Int, opacity:Float=1f,isPlating:Boolean=true,restartOnPlay:Boolean=true,iterations:Int=1){
+fun LottieAnimation( lottieAnim:Int, opacity:Float=1f,isPlating:Boolean=true,restartOnPlay:Boolean=true,iterations:Int=1,onComplete:()->Unit={}){
     val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(lottieAnim))
+    val progress by animateLottieCompositionAsState(composition)
+    LaunchedEffect(key1 = progress){
+        if(progress==1f){
+            onComplete()
+        }
+    }
 
     LottieAnimation(
         composition,
--- app/src/main/java/com/example/fon_classroommanagment_frontend/presentation/splash_screen/Splash_Screen.kt
+++ app/src/main/java/com/example/fon_classroommanagment_frontend/presentation/splash_screen/Splash_Screen.kt
@@ -14,26 +14,20 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.airbnb.lottie.compose.LottieConstants
 import com.example.fon_classroommanagment_frontend.common.Screen
 import com.example.fon_classroommanagment_frontend.presentation.common.bars.Components.LottieAnimation
 
 @Composable
 fun Splash_Screen(navController: NavHostController) {
 
-    var animate by remember{ mutableStateOf(false)}
-    val animateF = animateFloatAsState(targetValue = if(animate) 0f else 1f, animationSpec = tween(durationMillis = 3000))
-    LaunchedEffect(key1 = true){
-        animate=true
-    }
-    LaunchedEffect(key1 = animateF.value){
-        if(animateF.value==0f){
-            navController.navigate(Screen.LoginScreen.route)
-        }
-    }
+
+
     Box() {
         Image(
             painterResource(id = if (isSystemInDarkTheme()) R.drawable.bcg_spash_dark else R.drawable.bch_splash_light),
@@ -41,27 +35,14 @@ fun Splash_Screen(navController: NavHostController) {
             modifier = Modifier.fillMaxSize(),
             contentScale = ContentScale.Crop
         )
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                //.background(MaterialTheme.colorScheme.background)
-                .padding(10.dp), horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Box(contentAlignment = Alignment.TopCenter, modifier = Modifier.weight(1f)) {
-                Image(
-                    painter = painterResource(id = R.drawable.fonsplash),
-                    contentDescription = "Spalsh image",
-                    modifier = Modifier
-                        .fillMaxWidth(1f)
-                        .scale(animateF.value)
-                )
-            }
+
+
+              LottieAnimation(lottieAnim = R.raw.custom_anim_logo_fona, onComplete = {navController.navigate(Screen.LoginScreen.route)})
 
 
 
-            //Image(painter = painterResource(id = R.drawable.splash_img), contentDescription = "")
 
-            //LottieAnimation(lottieAnim = if(isSystemInDarkTheme()) R.raw.splash_animated_dark else R.raw.splash_animated_light)
-        }
+
+
     }
 }
