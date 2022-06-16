7ac8c0da7b2be9256b93aab35e898fdf34c55757
ilijaradojkovic <47418955+ilijaradojkovic@users.noreply.github.com>
2022-06-16 21:56:45 +0200

    Error handle za all reservation

--- app/src/main/java/com/example/fon_classroommanagment_frontend/presentation/all_reservation_screen/AllReservationViewModel.kt
+++ app/src/main/java/com/example/fon_classroommanagment_frontend/presentation/all_reservation_screen/AllReservationViewModel.kt
@@ -8,6 +8,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.fon_classroommanagment_frontend.common.Response
+import com.example.fon_classroommanagment_frontend.common.UIRequestResponse
 import com.example.fon_classroommanagment_frontend.data.Event
 import com.example.fon_classroommanagment_frontend.data.remote.dto.RequestAppointmetDateDTO
 import com.example.fon_classroommanagment_frontend.domain.use_case.GetReservationsForDateUseCse
@@ -28,6 +29,8 @@ class AllReservationViewModel @Inject constructor(private val getReservationsFor
     private val _reservationsForDay= mutableStateListOf<Event>()
     val reservationForDay = _reservationsForDay
 
+    private  var _uiState = mutableStateOf(UIRequestResponse())
+    val uiState= _uiState
     @RequiresApi(Build.VERSION_CODES.O)
     fun getReservationsForData(initialDate: LocalDate) {
         _reservationsForDay.clear()
@@ -37,14 +40,20 @@ class AllReservationViewModel @Inject constructor(private val getReservationsFor
             when(result){
                 is Response.Success->{
                     Log.i("cao",result.data.toString())
+                    _uiState.value=UIRequestResponse(success = true)
                     val transofmed=result.data?.map { x->Event(x.typeName,x.classroomName, TransformToLocalDate(x.Start_timeInHours),TransformToLocalDate(x.End_timeInHours)) }
                     if (transofmed != null) {
+
                         _reservationsForDay.addAll(transofmed)
                     }
 
                 }
-                is Response.Error->{Log.i("cao",result.message.toString())}
-                is Response.Loading->{Log.i("cao","loading")}
+                is Response.Error->{
+                    _uiState.value= UIRequestResponse(isError = true)
+                    Log.i("cao",result.message.toString())}
+                is Response.Loading->{
+                    _uiState.value=UIRequestResponse(isLoading = true)
+                    Log.i("cao","loading")}
             }
         }
             .launchIn(viewModelScope)
--- app/src/main/java/com/example/fon_classroommanagment_frontend/presentation/all_reservation_screen/AllReservations_Screen.kt
+++ app/src/main/java/com/example/fon_classroommanagment_frontend/presentation/all_reservation_screen/AllReservations_Screen.kt
@@ -3,19 +3,28 @@ package com.example.fon_classroommanagment_frontend.screens
 import android.annotation.SuppressLint
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
+import com.airbnb.lottie.compose.LottieConstants
 import com.example.fon_classroommanagment_frontend.CallendarPicker
+import com.example.fon_classroommanagment_frontend.R
 import com.example.fon_classroommanagment_frontend.presentation.common.bars.Components.Schedule
 import com.example.fon_classroommanagment_frontend.presentation.all_reservation_screen.AllReservationViewModel
+import com.example.fon_classroommanagment_frontend.presentation.common.bars.Components.LottieAnimation
 import com.foreverrafs.datepicker.state.rememberDatePickerState
 import java.time.LocalDate
 
@@ -31,8 +40,9 @@ fun AllReservations_Screen(
 
     val reservations =  allReservationViewModel.reservationForDay
 
+    val state by allReservationViewModel.uiState
 
-
+    val heightAnim = animateDpAsState(targetValue = if(state.isError) 100.dp else 0.dp)
     LaunchedEffect(key1 = true ){
         allReservationViewModel.getReservationsForData(LocalDate.now())
     }
@@ -40,6 +50,7 @@ fun AllReservations_Screen(
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
+                .animateContentSize(tween(1000))
         ) {
 
             Row(
@@ -49,6 +60,19 @@ fun AllReservations_Screen(
             ) {
                 CallendarPicker(){allReservationViewModel.getReservationsForData(it)}
             }
+
+
+
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+
+                    .height(heightAnim.value),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically){
+                    LottieAnimation(lottieAnim = R.raw.no_wifi, iterations = LottieConstants.IterateForever)
+
+                }
+
 
             Row(modifier = Modifier) {
                 Schedule(
