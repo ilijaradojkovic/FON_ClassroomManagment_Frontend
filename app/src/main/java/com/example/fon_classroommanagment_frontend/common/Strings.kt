03371eccb999cf305907dd4a90eb24c3a4f1c786
ilijaradojkovic <radojkovicika@gmail.com>
2022-07-04 14:03:10 +0200

    Dodate error poruke u jedinstvenu klasu

---
+++ app/src/main/java/com/example/fon_classroommanagment_frontend/common/Strings.kt
@@ -0,0 +1,40 @@
+package com.example.fon_classroommanagment_frontend.common
+
+object Strings {
+    const val name_error="Please enter valid name"
+    const val  name_error_explained_epmty="Name field cannot be empty"
+    const val  name_error_explained_max_length="Name field length must be less then 45"
+
+    const val reason_error="Please enter valid reason"
+    const val  reason_error_explained_epmty="Reason field cannot be empty"
+    const val  reason_error_explained_max_length="Reason field length must be less then 45"
+
+
+    const val description_error="Please enter valid description"
+    const val  description_error_explained_epmty="Description field cannot be empty"
+
+    const val classrooms_error="Please valid classrooms "
+    const val  classrooms_error_explained_epmty="Please select at least one classroom"
+
+    const val appointment_type_error="Please valid appointment type "
+    const val  appointment_type_error_explained_epmty="Please select appointment type"
+
+
+    const val end_time_error="Please enter valid end time"
+    const val end_time_error_explained_epmty="End time cant be empty"
+    const val end_time_error_explained_not_int="End time must be digit"
+  const val end_time_error_explained_num_digits="End time filed mush have 2 or les digits"
+  const val end_time_error_explained_not_working_at="We dont work at "
+  const val end_time_error_explained_after_start_time="Start time cant be after end time"
+
+    const val start_time_error="Please enter valid end time"
+    const val start_time_error_explained_epmty="Start time cant be empty"
+    const val start_time_error_explained_not_int="Start time must be digit"
+  const val start_time_error_explained_num_digits="Start time filed mush have 2 or les digits"
+  const val start_time_error_explained_not_working_at="We dont work at "
+
+    const val attendees_error="Please enter valid end time"
+    const val attendees_error_empty="Attendees filed cant be empty"
+    const val attendees_error_max_capacity="Attendees filed is over its max capacity"
+
+}
