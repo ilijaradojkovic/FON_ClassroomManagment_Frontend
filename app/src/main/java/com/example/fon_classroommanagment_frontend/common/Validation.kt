ca9c7b42aba39a30422fa58a61db0e802d2f4a2b
ilijaradojkovic <radojkovicika@gmail.com>
2022-07-04 13:57:03 +0200

    Objedinjavanje validacije u jednu klasu

--- app/src/main/java/com/example/fon_classroommanagment_frontend/common/Validation.kt
+++ app/src/main/java/com/example/fon_classroommanagment_frontend/common/Validation.kt
@@ -1,6 +1,6 @@
 package com.example.fon_classroommanagment_frontend.common
 
-sealed class Validation(val isSuccess:Boolean=false,val error:String?=null,val errorExplained:String?=null){
+sealed class Validation(val isSuccess:Boolean=false,val error:String="",val errorExplained:String=""){
 
     class Success(): Validation(isSuccess = true)
     class Error(message: String,errorExplained: String): Validation(error = message, errorExplained = errorExplained)
--- app/src/main/java/com/example/fon_classroommanagment_frontend/domain/use_case/appointment_insertion_page_cases/components/ValidateInsertionAppointmentUseCase.kt
+++ app/src/main/java/com/example/fon_classroommanagment_frontend/domain/use_case/appointment_insertion_page_cases/components/ValidateInsertionAppointmentUseCase.kt
@@ -1,5 +1,8 @@
 package com.example.fon_classroommanagment_frontend.domain.use_case.appointment_insertion_page_cases.components
 
+import com.example.fon_classroommanagment_frontend.data.remote.dto.InsertionAppointmentValidationDTO
+import com.example.fon_classroommanagment_frontend.data.remote.dto.ReserveDTO
+import com.example.fon_classroommanagment_frontend.data.remote.dto.ValidationInsertionResult
 import javax.inject.Inject
 
 class ValidateInsertionAppointmentUseCase @Inject constructor(
@@ -12,8 +15,16 @@ private val validateReason: ValidateReason,
 private val validateStartTime: ValidateStartTime,
 private  val validateAttendees: ValidateAttendees
 ) {
-    operator fun invoke()
+    operator fun invoke(insertionDTO: InsertionAppointmentValidationDTO): ValidationInsertionResult
     {
-//        validateName()
+        return ValidationInsertionResult(
+        validateName(insertionDTO.name),
+        validateDescription(insertionDTO.description),
+        validateReason(insertionDTO.reason),
+        validateStartTime(insertionDTO.startTime),
+        validateEndTime(insertionDTO.endTime,insertionDTO.startTime),
+        validateAppointmentType(insertionDTO.appointmentType),
+        validateClassrooms(insertionDTO.classrooms), validateAttendees(insertionDTO.numberOfAttendees),
+        )
     }
 }
